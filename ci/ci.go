f06ad65a3cad5ef138dc531c4d0386b611668c5f
tahir <tahir@tahirs-MacBook-Pro.local>
2020-11-15 20:17:54 -0500

    Updating CI logic

--- ci/ci.go
+++ ci/ci.go
@@ -1,26 +1,71 @@
 package main
 
 import (
+	"bufio"
+	ci "github.com/tahirali-csc/task-executor-ci"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func runCommand(cmd string) (string, error) {
-	out, err := exec.Command("/bin/sh", "-c", cmd).Output()
-	return string(out), err
+
+	cmdObj := exec.Command("/bin/sh", "-c", cmd)
+	reader, err := cmdObj.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	cmdObj.Start()
+	scanner := bufio.NewScanner(reader)
+	done := make(chan bool)
+	go func() {
+		for scanner.Scan() {
+			log.Println(scanner.Text())
+		}
+		done <- true
+	}()
+
+	<-done
+	err = cmdObj.Wait()
+	return "", err
+	// return string(out), err
 }
 
 func main() {
-	log.Println("Running Pipeline")
-	output, err := runCommand("cd ../ && go test -v -run TestRun1 hello-app/pkg/module1")
-	if err != nil {
-		panic(err)
-	}
-	log.Println(output)
+	log.Println("Starting Build...")
 
-	output, err = runCommand("cd ../ && go test -v -run TestRun2 hello-app/pkg/module1")
+	//For testing
+	os.Setenv("TE_HOST_URL", "http://localhost:8080")
+	os.Setenv("TE_BUILD_ID", "17")
+
+	//log.Println("Running Pipeline")
+	//output, err := runCommand("cd ../ && go test -v -run TestRun1 hello-app/pkg/module1")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//log.Println(output)
+	//
+	//output, err = runCommand("cd ../ && go test -v -run TestRun2 hello-app/pkg/module1")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//log.Println(output)
+
+	err := ci.RunStep(&ci.Step{
+		Name:  "unit-test",
+		Image: "alpine:latest",
+		Cmd:   []string{"sleep", "20s"},
+	})
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
-	log.Println(output)
+
+	err = ci.RunStep(&ci.Step{
+		Name:  "int-test",
+		Image: "alpine:latest",
+		Cmd:   []string{"date"},
+	})
+	if err != nil {
+		log.Println(err)
+	}
 }
--- ci/go.mod
+++ ci/go.mod
@@ -1,3 +1,7 @@
 module hello-app/ci
 
 go 1.14
+
+replace github.com/tahirali-csc/task-executor-ci => "/Users/tahir/workspace/rnd-projects/product launch/task-executor-ci"
+
+require github.com/tahirali-csc/task-executor-ci v0.0.0-20201115201126-058aec9f7f5e

79db7911f6435cbe37566fc2ffb8990c32479c99
tahir <tahir@tahirs-MacBook-Pro.local>
2020-11-19 17:59:01 -0500

    Updating code

--- ci/ci.go
+++ ci/ci.go
@@ -53,7 +53,7 @@ func main() {
 	err := ci.RunStep(&ci.Step{
 		Name:  "unit-test",
 		Image: "alpine:latest",
-		Cmd:   []string{"/bin/sh", "-c", "ls -al /tmp"},
+		Cmd:   []string{"/bin/sh", "-c", "date"},
 	})
 	if err != nil {
 		log.Println(err)
